5c43001965617fdac844d28b960a7baf75a85a3b
devexps <dev.experiences@gmail.com>
2023-09-28 17:21:01 +0700

    feat: unit-test

---
+++ mask/masker/masker_test.go
@@ -0,0 +1,575 @@
+package masker
+
+import "testing"
+
+var (
+	filteredLabel = "[filtered]"
+
+	MTest   = MType("test")
+	StrFunc = func(t MType, s string) (bool, string) {
+		if t == MTest {
+			return true, s + "-test"
+		}
+		return false, ""
+	}
+)
+
+func TestMasker_String(t *testing.T) {
+	type args struct {
+		t MType
+		s string
+	}
+	defaultMasker := NewMasker()
+	customMasker := NewMasker(WithMarkTypes(MName, MTest), WithStringFunc(StrFunc))
+	tests := []struct {
+		name   string
+		masker *Masker
+		args   args
+		want   string
+	}{
+		{
+			name:   "Error Mask Type",
+			masker: defaultMasker,
+			args: args{
+				t: MType(""),
+				s: "abcdefghi",
+			},
+			want: filteredLabel,
+		},
+		{
+			name:   "ID",
+			masker: defaultMasker,
+			args: args{
+				t: MID,
+				s: "ABC123456789",
+			},
+			want: "ABC123****",
+		},
+		{
+			name:   "Name",
+			masker: defaultMasker,
+			args: args{
+				t: MName,
+				s: "abcdefghi",
+			},
+			want: "a**defghi",
+		},
+		{
+			name:   "Password",
+			masker: defaultMasker,
+			args: args{
+				t: MPassword,
+				s: "abcdefghi",
+			},
+			want: "************",
+		},
+		{
+			name:   "Address",
+			masker: defaultMasker,
+			args: args{
+				t: MAddress,
+				s: "abcdefghi",
+			},
+			want: "abcdef******",
+		},
+		{
+			name:   "Email",
+			masker: defaultMasker,
+			args: args{
+				t: MEmail,
+				s: "abcd.company@gmail.com",
+			},
+			want: "abc****mpany@gmail.com",
+		},
+		{
+			name:   "Mobile",
+			masker: defaultMasker,
+			args: args{
+				t: MMobile,
+				s: "0987654321",
+			},
+			want: "0987***321",
+		},
+		{
+			name:   "Telephone",
+			masker: defaultMasker,
+			args: args{
+				t: MTelephone,
+				s: "0287654321",
+			},
+			want: "(02)8765-****",
+		},
+		{
+			name:   "URL",
+			masker: defaultMasker,
+			args: args{
+				t: MURL,
+				s: "http://admin:mysecretpassword@localhost:1234/uri",
+			},
+			want: "http://admin:xxxxx@localhost:1234/uri",
+		},
+		{
+			name:   "Custom MTest",
+			masker: customMasker,
+			args: args{
+				t: MTest,
+				s: "abc",
+			},
+			want: "abc-test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.masker.String(tt.args.t, tt.args.s, filteredLabel); got != tt.want {
+				t.Errorf("Masker.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMasker_overlay(t *testing.T) {
+	type args struct {
+		str     string
+		overlay string
+		start   int
+		end     int
+	}
+	defaultMasker := NewMasker()
+	tests := []struct {
+		name          string
+		masker        *Masker
+		args          args
+		wantOverlayed string
+	}{
+		{
+			name:   "Empty Input",
+			masker: defaultMasker,
+			args: args{
+				str:     "",
+				overlay: "*",
+				start:   0,
+				end:     0,
+			},
+			wantOverlayed: "",
+		},
+		{
+			name:   "Happy Pass",
+			masker: defaultMasker,
+			args: args{
+				str:     "abcdefg",
+				overlay: "***",
+				start:   1,
+				end:     5,
+			},
+			wantOverlayed: "a***fg",
+		},
+		{
+			name:   "Start Less Than 0",
+			masker: defaultMasker,
+			args: args{
+				str:     "abcdefg",
+				overlay: "***",
+				start:   -1,
+				end:     5,
+			},
+			wantOverlayed: "***fg",
+		},
+		{
+			name:   "Start Greater Than Length",
+			masker: defaultMasker,
+			args: args{
+				str:     "abcdefg",
+				overlay: "***",
+				start:   30,
+				end:     31,
+			},
+			wantOverlayed: "abcdefg***",
+		},
+		{
+			name:   "End Less Than 0",
+			masker: defaultMasker,
+			args: args{
+				str:     "abcdefg",
+				overlay: "***",
+				start:   1,
+				end:     -5,
+			},
+			wantOverlayed: "***bcdefg",
+		},
+		{
+			name:   "Start Less Than End",
+			masker: defaultMasker,
+			args: args{
+				str:     "abcdefg",
+				overlay: "***",
+				start:   5,
+				end:     1,
+			},
+			wantOverlayed: "a***fg",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.masker.overlay(tt.args.str, tt.args.overlay, tt.args.start, tt.args.end); got != tt.wantOverlayed {
+				t.Errorf("Masker.overlay() = %v, want %v", got, tt.wantOverlayed)
+			}
+		})
+	}
+}
+
+func TestID(t *testing.T) {
+	type args struct {
+		i string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Empty Input",
+			args: args{
+				i: "",
+			},
+			want: "",
+		},
+		{
+			name: "Happy Pass",
+			args: args{
+				i: "A123456789",
+			},
+			want: "A12345****",
+		},
+		{
+			name: "Length Less Than 6",
+			args: args{
+				i: "A12",
+			},
+			want: "A12****",
+		},
+		{
+			name: "Length Less Than 6",
+			args: args{
+				i: "A",
+			},
+			want: "A****",
+		},
+		{
+			name: "Length Between 6 and 10",
+			args: args{
+				i: "A123456",
+			},
+			want: "A12345****",
+		},
+	}
+	for _, tt := range tests {
+		m := NewMasker()
+		t.Run(tt.name, func(t *testing.T) {
+			if got := m.ID(tt.args.i); got != tt.want {
+				t.Errorf("ID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestName(t *testing.T) {
+	type args struct {
+		i string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Empty Input",
+			args: args{
+				i: "",
+			},
+			want: "",
+		},
+		{
+			name: "Length 1",
+			args: args{
+				i: "A",
+			},
+			want: "**",
+		},
+		{
+			name: "Length 2",
+			args: args{
+				i: "AB",
+			},
+			want: "A**",
+		},
+		{
+			name: "Length 3",
+			args: args{
+				i: "ABC",
+			},
+			want: "A**C",
+		},
+		{
+			name: "Length 4",
+			args: args{
+				i: "ABCD",
+			},
+			want: "A**D",
+		},
+		{
+			name: "Length 5",
+			args: args{
+				i: "ABCDE",
+			},
+			want: "A**DE",
+		},
+		{
+			name: "Length 6",
+			args: args{
+				i: "ABCDEF",
+			},
+			want: "A**DEF",
+		},
+		{
+			name: "English Full Name",
+			args: args{
+				i: "Jorge Marry",
+			},
+			want: "J**ge M**ry",
+		},
+	}
+	for _, tt := range tests {
+		m := NewMasker()
+		t.Run(tt.name, func(t *testing.T) {
+			if got := m.Name(tt.args.i); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPassword(t *testing.T) {
+	type args struct {
+		i string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Empty Input",
+			args: args{
+				i: "",
+			},
+			want: "",
+		},
+		{
+			name: "Happy Pass",
+			args: args{
+				i: "1234567",
+			},
+			want: "************",
+		},
+		{
+			name: "Happy Pass",
+			args: args{
+				i: "abcd!@#$%321",
+			},
+			want: "************",
+		},
+	}
+	for _, tt := range tests {
+		m := NewMasker()
+		t.Run(tt.name, func(t *testing.T) {
+			if got := m.Password(tt.args.i); got != tt.want {
+				t.Errorf("Password() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAddress(t *testing.T) {
+	type args struct {
+		i string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Empty Input",
+			args: args{
+				i: "",
+			},
+			want: "",
+		},
+		{
+			name: "Long Address",
+			args: args{
+				i: "1 AB Paradise Road",
+			},
+			want: "1 AB P******",
+		},
+		{
+			name: "Length Less Than 6",
+			args: args{
+				i: "ABC",
+			},
+			want: "******",
+		},
+	}
+	for _, tt := range tests {
+		m := NewMasker()
+		t.Run(tt.name, func(t *testing.T) {
+			if got := m.Address(tt.args.i); got != tt.want {
+				t.Errorf("Address() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEmail(t *testing.T) {
+	type args struct {
+		i string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Empty Input",
+			args: args{
+				i: "",
+			},
+			want: "",
+		},
+		{
+			name: "Empty @",
+			args: args{
+				i: "ggw.changgmail.com",
+			},
+			want: "ggw****nggmail.com",
+		},
+		{
+			name: "Happy Pass",
+			args: args{
+				i: "ggw.chang@gmail.com",
+			},
+			want: "ggw****ng@gmail.com",
+		},
+		{
+			name: "Address Less Than 3",
+			args: args{
+				i: "qq@gmail.com",
+			},
+			want: "qq****@gmail.com",
+		},
+	}
+	for _, tt := range tests {
+		m := NewMasker()
+		t.Run(tt.name, func(t *testing.T) {
+			if got := m.Email(tt.args.i); got != tt.want {
+				t.Errorf("Email() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMobile(t *testing.T) {
+	type args struct {
+		i string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Empty Input",
+			args: args{
+				i: "",
+			},
+			want: "",
+		},
+		{
+			name: "Happy Pass",
+			args: args{
+				i: "0978978978",
+			},
+			want: "0978***978",
+		},
+		{
+			name: "Happy Pass",
+			args: args{
+				i: "0912345678",
+			},
+			want: "0912***678",
+		},
+	}
+	for _, tt := range tests {
+		m := NewMasker()
+		t.Run(tt.name, func(t *testing.T) {
+			if got := m.Mobile(tt.args.i); got != tt.want {
+				t.Errorf("Mobile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTelephone(t *testing.T) {
+	type args struct {
+		i string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Empty Input",
+			args: args{
+				i: "",
+			},
+			want: "",
+		},
+		{
+			name: "Out of range",
+			args: args{
+				i: "(02-)27-99-3--078-4325",
+			},
+			want: "02279930784325",
+		},
+		{
+			name: "With Special Chart",
+			args: args{
+				i: "(02-)27   99-3--078",
+			},
+			want: "(02)2799-****",
+		},
+		{
+			name: "Happy Pass",
+			args: args{
+				i: "0227993078",
+			},
+			want: "(02)2799-****",
+		},
+		{
+			name: "Happy Pass",
+			args: args{
+				i: "0788079966",
+			},
+			want: "(07)8807-****",
+		},
+	}
+	for _, tt := range tests {
+		m := NewMasker()
+		t.Run(tt.name, func(t *testing.T) {
+			if got := m.Telephone(tt.args.i); got != tt.want {
+				t.Errorf("Telephone() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ mask/masking_test.go
@@ -0,0 +1,361 @@
+package mask
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/devexps/go-pkg/v2/mask/masker"
+)
+
+func TestTagFilter(t *testing.T) {
+	t.Run("default ", func(t *testing.T) {
+		type myRecord struct {
+			ID    string
+			EMail *string `mask:"secret"`
+		}
+		e := "dummy@dummy.com"
+		record := myRecord{
+			ID:    "userId",
+			EMail: &e,
+		}
+		maskTool := New(TagFilter())
+		filteredData := maskTool.Apply(record)
+		assert.NotNil(t, filteredData)
+		copied, ok := filteredData.(myRecord)
+		require.True(t, ok)
+		require.NotNil(t, copied)
+		assert.Equal(t, record.EMail, copied.EMail)
+		assert.Equal(t, record.ID, copied.ID)
+	})
+
+	t.Run("custom ", func(t *testing.T) {
+		type myRecord struct {
+			ID    string
+			EMail string `mask:"email"`
+			Token string `mask:"secret"`
+		}
+		record := myRecord{
+			ID:    "userId",
+			EMail: "dummy@dummy.com",
+			Token: "abcd1234",
+		}
+		maskTool := New(TagsFilter())
+		filteredData := maskTool.Apply(record)
+		assert.NotNil(t, filteredData)
+		copied, ok := filteredData.(myRecord)
+		require.True(t, ok)
+		require.NotNil(t, copied)
+		assert.Equal(t, "dum****@dummy.com", copied.EMail)
+		assert.Equal(t, filteredLabel, copied.Token)
+		assert.Equal(t, record.ID, copied.ID)
+	})
+}
+
+func TestFieldFilter(t *testing.T) {
+	t.Run("default", func(*testing.T) {
+		type myRecord struct {
+			ID    string
+			Phone string
+			Email string
+		}
+		record := myRecord{
+			ID:    "userId",
+			Phone: "090-0000-0000",
+			Email: "abc@gmail.com",
+		}
+		maskTool := New(FieldFilter("Phone"), FieldFilter("Email", masker.MEmail))
+		filteredData := maskTool.Apply(record)
+		require.NotNil(t, filteredData)
+		copied, ok := filteredData.(myRecord)
+		require.True(t, ok)
+		require.NotNil(t, copied)
+		assert.Equal(t, filteredLabel, copied.Phone)
+		assert.Equal(t, "abc****@gmail.com", copied.Email)
+		assert.Equal(t, record.ID, copied.ID)
+	})
+}
+
+func TestFieldPrefixFilter(t *testing.T) {
+	t.Run("default", func(*testing.T) {
+		type myRecord struct {
+			ID          string
+			SecurePhone string
+			SecureEmail string
+		}
+		record := myRecord{
+			ID:          "userId",
+			SecurePhone: "090-0000-0000",
+			SecureEmail: "abc.def@gmail.com",
+		}
+		maskTool := New(FieldPrefixFilter("Secure", masker.MMobile))
+		filteredData := maskTool.Apply(record)
+		require.NotNil(t, filteredData)
+		copied, ok := filteredData.(myRecord)
+		require.True(t, ok)
+		require.NotNil(t, copied)
+		assert.Equal(t, "090-***0-0000", copied.SecurePhone)
+		assert.Equal(t, "abc.***@gmail.com", copied.SecureEmail)
+		assert.Equal(t, record.ID, copied.ID)
+	})
+}
+
+func TestFieldRegexFilter(t *testing.T) {
+	type myRecord struct {
+		ID    string
+		Link  string
+		Link1 string
+	}
+	customRegex := "^https:\\/\\/(dummy-backend.)[0-9a-z]*.com\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$"
+
+	t.Run("default", func(*testing.T) {
+		record := myRecord{
+			ID:    "userId",
+			Link:  "https://dummy-backend.dummy.com/v2/random",
+			Link1: "https://dummy-frontend.dummy.com/v2/random",
+		}
+		maskTool := New(RegexFilter(customRegex))
+		filteredData := maskTool.Apply(record)
+		require.NotNil(t, filteredData)
+		copied, ok := filteredData.(myRecord)
+		require.True(t, ok)
+		require.NotNil(t, copied)
+		assert.Equal(t, filteredLabel, copied.Link)
+		assert.Equal(t, record.Link1, copied.Link1)
+		assert.Equal(t, record.ID, copied.ID)
+	})
+
+	t.Run("masking", func(*testing.T) {
+		record := myRecord{
+			ID:    "userId",
+			Link:  "https://dummy-backend.dummy.com/v2/random",
+			Link1: "https://dummy-frontend.dummy.com/v2/random",
+		}
+		maskTool := New(RegexFilter(customRegex, masker.MPassword))
+		filteredData := maskTool.Apply(record)
+		require.NotNil(t, filteredData)
+		copied, ok := filteredData.(myRecord)
+		require.True(t, ok)
+		require.NotNil(t, copied)
+		assert.Equal(t, "************", copied.Link)
+		assert.Equal(t, record.Link1, copied.Link1)
+		assert.Equal(t, record.ID, copied.ID)
+	})
+}
+
+func TestValueFilter(t *testing.T) {
+	t.Run("DefaultValueFilter", func(t *testing.T) {
+		const issuedToken = "abcd1234"
+		maskTool := New(ValueFilter(issuedToken))
+
+		t.Run("string", func(t *testing.T) {
+			record := "Authorization: Bearer " + issuedToken
+			filteredData := maskTool.Apply(record)
+			require.NotNil(t, filteredData)
+			assert.Equal(t, "Authorization: Bearer [filtered]", filteredData)
+		})
+		t.Run("struct", func(t *testing.T) {
+			type myRecord struct {
+				ID   string
+				Data string
+			}
+			record := myRecord{
+				ID:   "userId",
+				Data: issuedToken,
+			}
+
+			filteredData := maskTool.Apply(record)
+			require.NotNil(t, filteredData)
+			copied, ok := filteredData.(myRecord)
+			require.True(t, ok)
+			require.NotNil(t, copied)
+			assert.Equal(t, record.ID, copied.ID)
+			assert.Equal(t, filteredLabel, copied.Data)
+		})
+		t.Run("array", func(t *testing.T) {
+			record := []string{
+				"userId",
+				"data",
+				issuedToken,
+			}
+			filteredData := maskTool.Apply(record)
+			require.NotNil(t, filteredData)
+			assert.Equal(t, []string([]string{"userId", "data", filteredLabel}), filteredData)
+		})
+		t.Run("map", func(*testing.T) {
+			mapRecord := map[string]interface{}{
+				"data": issuedToken,
+			}
+			filteredData := maskTool.Apply(mapRecord)
+			require.NotNil(t, filteredData)
+			assert.Equal(t, map[string]interface{}{"data": "[filtered]"}, filteredData)
+		})
+	})
+	t.Run("CustomValueFilter", func(t *testing.T) {
+		const issuedToken = "abcd1234"
+		maskTool := New(ValueFilter(issuedToken, masker.MPassword))
+		t.Run("string", func(t *testing.T) {
+			record := "Authorization: Bearer " + issuedToken
+			filteredData := maskTool.Apply(record)
+			require.NotNil(t, filteredData)
+			assert.Equal(t, "Authorization: Bearer ************", filteredData)
+		})
+	})
+	t.Run("OtherValueFilter", func(t *testing.T) {
+		customMasker := New(
+			ValueFilter("blue"),
+		)
+		type testData struct {
+			ID    int
+			Name  string
+			Label string
+		}
+		t.Run("non-ptr struct can be modified", func(t *testing.T) {
+			data := testData{
+				Name:  "blue",
+				Label: "five",
+			}
+			v := customMasker.Apply(data)
+			require.NotNil(t, v)
+			copied, ok := v.(testData)
+			require.True(t, ok)
+			require.NotNil(t, copied)
+			assert.Equal(t, filteredLabel, copied.Name)
+			assert.Equal(t, "five", copied.Label)
+		})
+		t.Run("original data is not modified when filtered", func(t *testing.T) {
+			data := &testData{
+				ID:    100,
+				Name:  "blue",
+				Label: "five",
+			}
+			v := customMasker.Apply(data)
+			require.NotNil(t, v)
+			copied, ok := v.(*testData)
+			require.True(t, ok)
+			require.NotNil(t, copied)
+			assert.Equal(t, filteredLabel, copied.Name)
+			assert.Equal(t, "blue", data.Name)
+			assert.Equal(t, "five", data.Label)
+			assert.Equal(t, "five", copied.Label)
+			assert.Equal(t, 100, copied.ID)
+		})
+		t.Run("nested structure can be modified", func(t *testing.T) {
+			type testDataParent struct {
+				Child testData
+			}
+			data := &testDataParent{
+				Child: testData{
+					Name:  "blue",
+					Label: "five",
+				},
+			}
+			v := customMasker.Apply(data)
+			require.NotNil(t, v)
+			copied, ok := v.(*testDataParent)
+			require.True(t, ok)
+			require.NotNil(t, copied)
+			assert.Equal(t, filteredLabel, copied.Child.Name)
+			assert.Equal(t, "five", copied.Child.Label)
+		})
+		t.Run("map data", func(t *testing.T) {
+			data := map[string]*testData{
+				"xyz": {
+					Name:  "blue",
+					Label: "five",
+				},
+			}
+			v := customMasker.Apply(data)
+			require.NotNil(t, v)
+			copied, ok := v.(map[string]*testData)
+			require.True(t, ok)
+			require.NotNil(t, copied)
+			assert.Equal(t, filteredLabel, copied["xyz"].Name)
+			assert.Equal(t, "five", copied["xyz"].Label)
+		})
+		t.Run("array data with ptr", func(t *testing.T) {
+			data := []*testData{
+				{
+					Name:  "orange",
+					Label: "five",
+				},
+				{
+					Name:  "blue",
+					Label: "five",
+				},
+			}
+			v := customMasker.Apply(data)
+			require.NotNil(t, v)
+			copied, ok := v.([]*testData)
+			require.True(t, ok)
+			require.NotNil(t, copied)
+			assert.Equal(t, "orange", copied[0].Name)
+			assert.Equal(t, filteredLabel, copied[1].Name)
+			assert.Equal(t, "five", copied[1].Label)
+		})
+	})
+}
+
+func TestTypeFilter(t *testing.T) {
+	type password string
+	type array []int32
+
+	t.Run("CustomTypeFilter", func(t *testing.T) {
+		type myRecord struct {
+			ID       string
+			Password password
+		}
+		record := myRecord{
+			ID:       "userId",
+			Password: "abcd1234",
+		}
+		t.Run("Type Filter with Mask Type", func(t *testing.T) {
+			maskTool := New(TypeFilter(password(""), masker.MPassword))
+			filteredData := maskTool.Apply(record)
+			require.NotNil(t, filteredData)
+			copied, ok := filteredData.(myRecord)
+			require.True(t, ok)
+			require.NotNil(t, copied)
+			assert.Equal(t, password("************"), copied.Password)
+			assert.Equal(t, record.ID, copied.ID)
+
+		})
+	})
+	t.Run("TypeFilter", func(t *testing.T) {
+		type myRecord struct {
+			ID       string
+			Password password
+			Ages     array
+		}
+		record := myRecord{
+			ID:       "userId",
+			Password: "abcd1234",
+			Ages:     array{10},
+		}
+		t.Run("Default Type Filter", func(t *testing.T) {
+			maskTool := New(TypeFilter(password("")), TypeFilter(array{}))
+			filteredData := maskTool.Apply(record)
+			require.NotNil(t, filteredData)
+			copied, ok := filteredData.(myRecord)
+			require.True(t, ok)
+			require.NotNil(t, copied)
+			assert.Equal(t, password(filteredLabel), copied.Password)
+			assert.Equal(t, array(nil), copied.Ages)
+			assert.Equal(t, record.ID, copied.ID)
+		})
+	})
+}
+
+func TestCustomMasker(t *testing.T) {
+	t.Run("default", func(t *testing.T) {
+		SetMasker(masker.NewMasker(masker.WithMarkTypes("test")))
+		assert.NotNil(t, maskerInstance)
+		assert.Equal(t, 10, len(maskerInstance.MarkTypes()))
+
+		maskTool := New()
+		assert.NotNil(t, maskTool)
+		filteredData := maskTool.Apply(nil)
+		assert.Nil(t, filteredData)
+	})
+}
